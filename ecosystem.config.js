e2446387483cc71dfaf9f2ef87a3d0788c1579a6
Johandika <johanelyosse@gmail.com>
2025-09-25 11:02:14 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: 'PA28-API-Test',    // A name for your application
+    script: 'app.js',             // The entry point of your app
+    instances: '1',             // Run on all available CPU cores
+    exec_mode: 'fork',         // Enable cluster mode
+    watch: false,                 // Disable watching files (for production)
+    env_production: {             // Environment variables for production
+      "NODE_ENV": "development",
+    }
+  }]
+};
